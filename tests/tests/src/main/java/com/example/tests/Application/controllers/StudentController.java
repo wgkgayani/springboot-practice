7c7b9c45c98b2e69277f71f51dc9f8b1bdaf82d4
wgkgayani <kaveeshagayani21@gmail.com>
2026-06-05 22:22:39 +0530

    Add three layers and add simple API

---
+++ tests/tests/src/main/java/com/example/tests/Application/controllers/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.tests.Application.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @GetMapping("/list")
+    public String getStudent(){
+        return "Student List";
+    }
+
+    @PostMapping("/list")
+    public String addStudent(){
+        return "Student Added";
+    }
+}
