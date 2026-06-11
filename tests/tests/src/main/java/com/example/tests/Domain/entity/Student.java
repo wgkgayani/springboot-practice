3b826422fa1a773e266b1fa11e77a2ee8f4d6600
wgkgayani <kaveeshagayani21@gmail.com>
2026-06-11 08:02:38 +0530

    Use annotations

--- tests/tests/src/main/java/com/example/tests/Domain/entity/Student.java
+++ tests/tests/src/main/java/com/example/tests/Domain/entity/Student.java
@@ -3,9 +3,15 @@ package com.example.tests.Domain.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "student")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Student {
 
     @Id
@@ -14,50 +20,5 @@ public class Student {
     private String name;
     private String address;
     private String email;
-    
-
-    // Empty constructor REQUIRED for JPA
-    public Student() {
-
-    }
-
-    // Parameterized constructor
-    public Student(Integer id, String name, String address, String email) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.email = email;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private Integer grade;
 }
